151bc24f45e8b0cc78d966b8ae927ea27e6f4bf3
Jose-Velasco <jose.juarez.velasco@gmail.com>
2023-07-26 19:59:05 -0700

    dbmanger class now get db info from settings class,created media APIRouter, implmented create and get media by id db operations

--- backend/app/api_routes.py
+++ backend/app/api_routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.routes.authentication import auth_router
+from app.routes.media import media_router
 
 # registration of all system routes
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth", tags=["authentication"])
+api_router.include_router(media_router, prefix="/media", tags=["media"])
