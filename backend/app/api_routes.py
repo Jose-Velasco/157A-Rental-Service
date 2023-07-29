f7b131e855993d5d96fb7394fa7b2080beb9c6ed
Dawson-N-W <Dawson.n.williams@gmail.com>
2023-07-28 17:55:21 -0700

    rented endpoints

--- backend/app/api_routes.py
+++ backend/app/api_routes.py
@@ -4,6 +4,7 @@ from app.routes.transaction_route import transaction_router
 from app.routes.user_route import user_router
 from app.routes.media_route import media_router
 from app.routes.inventory_route import inventory_router
+from app.routes.rented_route import rented_router
 
 
 # registration of all system routes
@@ -18,3 +19,5 @@ api_router.include_router(media_router, prefix="/media", tags=["media"])
 api_router.include_router(inventory_router, prefix="/inv", tags=["inventory"])
 
 api_router.include_router(transaction_router, prefix="/tran", tags=["transaction"])
+
+api_router.include_router(rented_router, prefix="/rent", tags=["rented"])
