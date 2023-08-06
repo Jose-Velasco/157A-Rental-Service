024a71bc195353740536d45f8d29ed5600a05814
Perfect1076 <jeffreyvan.2001@gmail.com>
2023-08-05 21:50:33 -0700

    Update UpdateUser.tsx

--- frontend/src/pages/UpdateUser.tsx
+++ frontend/src/pages/UpdateUser.tsx
@@ -75,16 +75,13 @@ const UpdateUser: React.FC = () => {
     const initializeProfile = () => {
         setFirstName(getUser?.first_name ? getUser.first_name : "");
         setLastName(getUser?.last_name ? getUser.last_name : "");
-        setBirthday(getUser?.birthday ? getUser.birthday : Date);
-        setPhoneNumber(getUser?.phone_number ? getUser.phone_number : "");
+        setPhoneNumber(getUser?.phone_number ? getUser.phone_number : 0);
         setStreetAddress(getUser?.address[0].street ? getUser.address[0].street : "");
         setCity(getUser?.address[0].city ? getUser.address[0].city : "");
-        setZipCode(getUser?.address[0].zip_code ? getUser.address[0].zip_code : "");
+        setZipCode(getUser?.address[0].zip_code ? getUser.address[0].zip_code : 0);
         setCountry(getUser?.address[0].country ? getUser.address[0].country : "");
         setState(getUser?.address[0].state ? getUser.address[0].state : "");
         setEmail(getUser?.email[0].email ? getUser.email[0].email : "");
-        setUserName(getUser?.username ? getUser.username : "");
-        setPassword(getUser?.password ? getUser.password : "");
     };
 
 
@@ -93,20 +90,19 @@ const UpdateUser: React.FC = () => {
          <div style={{marginBottom: 75}}>
                 <ReusableBar title = {"Update "+ `${getUser?.first_name}`} showInventoryIcon = {false} />
             </div>
+            initializeProfile();
 
             <Typography align="center" gutterBottom fontSize={35}> Create Account</Typography>
             <TextField label = "First Name" type = "text" value ={firstName} onChange={(e) => setFirstName(e.target.value)}fullWidth/>
             <TextField label = "Last Name*" type = "text"value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth/>
             <TextField label = "Phone Number" type = "number"value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} fullWidth/>
-            <TextField label = "Birthday" type = "date" fullWidth value={birthday} onChange={(e) => setBirthday(e.target.value)}/>
+            <TextField label = "Birthday" type = "Date" fullWidth value={birthday} onChange={(e) => setBirthday(e.target.value)}/>
             <TextField label = "Street Address" type = "text"value={streetAddress} onChange={(e) => setStreetAddress(e.target.value)} fullWidth/>
             <TextField label = "City" type = "text" fullWidth value={city} onChange={(e) => setCity(e.target.value)}/>
             <TextField label = "State" type = "text" fullWidth value={state} onChange={(e) => setState(e.target.value)}/>
             <TextField label = "Zip Code" type ="number" fullWidth value ={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
             <TextField label = "Country" type ="text" fullWidth value ={country} onChange={(e) => setCountry(e.target.value)}/>
             <TextField label = "Email" type = "email" value ={email} onChange={(e) => setEmail(e.target.value)}fullWidth />
-            <TextField label = "Username" type = "text" fullWidth value ={userName} onChange={(e) => setUserName(e.target.value)}/>
-            <TextField label = "Password" type = "password" fullWidth value ={password} onChange={(e) => setPassword(e.target.value)}/>
             <Button type = "submit" fullWidth variant = "contained" color = "primary" sx={{mt:3,mb:2}} onClick = {handleSubmit}>Update Profile!</Button>
 
 
